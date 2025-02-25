d120e75ad9f0ee60a32e57acd2f62d9aa663c11d
Oleksandr <oleks.bodnar@gmail.com>
2025-02-25 13:41:32 +0200

    Init

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
-# goit-algo2-hw-01
-
-## Пошук максимального та мінімального елементів
-
-```
 def find_min_max(arr):
     if len(arr) == 1:
         return arr[0], arr[0]
-
+    
     if len(arr) == 2:
         return (arr[0], arr[1]) if arr[0] < arr[1] else (arr[1], arr[0])
-
+    
     mid = len(arr) // 2
     left_min, left_max = find_min_max(arr[:mid])
     right_min, right_max = find_min_max(arr[mid:])
-
+    
     return min(left_min, right_min), max(left_max, right_max)
-```
 
-## Пошук k-го найменшого елемента
 
-```
 def quick_select(arr, k):
     if len(arr) == 1:
         return arr[0]
-
+    
     pivot = arr[len(arr) // 2]
     left = [x for x in arr if x < pivot]
     mid = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
-
+    
     if k <= len(left):
         return quick_select(left, k)
     elif k <= len(left) + len(mid):
         return pivot
     else:
         return quick_select(right, k - len(left) - len(mid))
-```
+
+arr = [3, 1, 7, 5, 9, 2, -5, 0, 8, 22, 47]
+print(find_min_max(arr)) 
+
+k = 3 # 3-й найменший елемент
+print(quick_select(arr, k))
